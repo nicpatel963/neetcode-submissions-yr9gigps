2671773850064fb79a0804689841d88ad2538d96
Nirmal patel <51151608+nicpatel963@users.noreply.github.com>
2026-04-09 10:55:03 -0600

    Add: is-anagram - submission-11

---
+++ Data Structures & Algorithms/is-anagram/submission-11.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        char = [0] * 26
+
+        for i in range(len(s)):
+            char[ord(s[i])-ord('a')] += 1
+            char[ord(t[i])-ord('a')] -= 1
+        
+        for i in char:
+            if i != 0:
+                return False
+        return True
