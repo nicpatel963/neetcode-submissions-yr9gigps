73c84b4fc62eb43f9ff3159b31821666723b1f8f
Nirmal patel <51151608+nicpatel963@users.noreply.github.com>
2026-04-22 11:01:20 -0600

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.py
@@ -0,0 +1,22 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        s1Freq = {}
+        for i in s1:
+            s1Freq[i] = s1Freq.get(i,0) + 1
+        k = len(s1)
+        s2Freq = {}
+        l = 0
+        
+        for i in range(len(s2)):
+            s2Freq[s2[i]] = s2Freq.get(s2[i], 0) + 1
+            while i-l+1 > k:
+                if s2Freq[s2[l]] > 1:
+                    s2Freq[s2[l]] = s2Freq[s2[l]] - 1
+                else:
+                    s2Freq.pop(s2[l])
+                l += 1
+            print(s2Freq)
+            if s1Freq == s2Freq:
+                return True
+            
+        return False
